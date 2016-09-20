55c28fe5a3246fbac8101bf9b01cb65f42635cee
Timothy Forste <tforste@gmail.com>
2016-09-19 23:46:03 -0700

    Created the Bloth Prefab
    Created the bolt prefeb including capsule collider and script for
    movement of the lazer

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+    public float speed;
+
+    //This code is executed on the very first frame this object is instantiated
+    void Start ()
+    {
+        Rigidbody projectileRB = GetComponent<Rigidbody>();
+
+        projectileRB.velocity = transform.forward * speed;
+    }
+
+}
