0ec7dcf2ba99b7341e88075f724383393f31ed81
Timothy Forste <tforste@gmail.com>
2016-09-26 22:03:30 -0700

    Created Boundry
    Added Boundry Game object and DestroyByBoundry Script

---
+++ Assets/Scripts/DestroyByBoundry.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundry : MonoBehaviour {
+
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
